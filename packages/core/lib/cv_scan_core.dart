d7e086f06ec92e43d91e355b231697076ed13b6c
Ermek <ermek.akkmatov@gmail.com>
2026-06-22 20:09:29 +0600

    feat: Update VSCode settings for Dart formatting and file associations; adjust exports in cv_scan_core.dart

--- packages/core/lib/cv_scan_core.dart
+++ packages/core/lib/cv_scan_core.dart
@@ -1,4 +1,4 @@
-export 'src/config/app_environment.dart';
 export 'src/config/api_config.dart';
-export 'src/config/pagination_config.dart';
+export 'src/config/app_environment.dart';
 export 'src/config/config.dart';
+export 'src/config/pagination_config.dart';
