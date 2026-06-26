d75be7aa3c13a6cfc0bbb42b27a6315c9ed93b20
Ermek <ermek.akkmatov@gmail.com>
2026-06-26 07:23:45 +0600

    feat: Update CandidatesListPage to include a TextFormField for user input

--- packages/features/candidates_list/lib/src/view/candidates_list_page.dart
+++ packages/features/candidates_list/lib/src/view/candidates_list_page.dart
@@ -4,5 +4,14 @@ class CandidatesListPage extends StatelessWidget {
   const CandidatesListPage({super.key});
 
   @override
-  Widget build(BuildContext context) => const Scaffold(body: Center(child: Text('Candidates')));
+  Widget build(BuildContext context) => Scaffold(
+    body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        spacing: 8,
+        children: [const Text('Candidates'), TextFormField()],
+      ),
+    ),
+  );
 }
